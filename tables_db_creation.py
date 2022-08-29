3818a6f99d0b25e74bf5ed2f3826749987733337
rikardoroa <rikardoroa@hotmail.com>
2022-08-28 23:27:48 -0500

    Update tables_db_creation.py

--- tables_db_creation.py
+++ tables_db_creation.py
@@ -19,7 +19,6 @@ config.sections()
 class db_tables:
 
     def __init__(self):
-      
         self.conn_db2 = None
         self.conn_db1 = psycopg2.connect(database='postgres', user=os.getenv('user'),
                                          password=os.getenv('pass'), host=os.getenv('host'), port='5432')
@@ -28,9 +27,7 @@ class db_tables:
         self.query5 = config.get('main', 'q5')
 
     def database_creation(self):
-
         """ this function creates a database named COINS_DETAIL """
-
         try:
             print("----please wait a moment..verifying or creating databases and tables..---")
             self.conn_db1.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
@@ -48,12 +45,10 @@ class db_tables:
             logger.debug("Databases as tables created successfully!")
 
     def tables_creation(self):
-
         """ this function creates two tables, named COINS_DETAIL
             and COINS_DETAIL_PRICE where the data will be uploaded
             into the database COINS_DETAIL
         """
-
         try:
 
             self.conn_db2 = psycopg2.connect(database='coins_detail', user=os.getenv('user'), password=os.getenv('pass'),
@@ -64,7 +59,6 @@ class db_tables:
             cursor_.execute(self.query2)
             cursor_.close()
             return self.conn_db2
-
         except Exception:
             logger.warning("Database and table already created!")
         except (KeyboardInterrupt, EOFError):
@@ -75,7 +69,6 @@ class db_tables:
             logger.debug("Databases as tables created successfully!")
 
     def average_price_per_coin(self):
-
         """function that calls a query to insert the price average
            per coin and per month on the coins_price_detail table
         """
